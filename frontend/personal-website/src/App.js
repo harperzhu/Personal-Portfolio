d479f7c09dbfe3294a4f5ac1b8579859c2e5724d
Harperzhu <harperzhu@yahoo.com>
2023-05-26 16:42:38 -0700

    fix the style in main page

--- frontend/personal-website/src/App.js
+++ frontend/personal-website/src/App.js
@@ -1,5 +1,5 @@
 // import logo from "./logo.svg";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // import React, { useState } from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
@@ -11,14 +11,6 @@ import "../src/assets/css/style.css";
 import NavBar from "./components/NavBar";
 import ErrorPage from "./components/ErrorPage";
 
-{
-  /* <link rel="shortcut icon" href="./assets/images/logo.ico" type="image/x-icon">
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet"> */
-}
-
 function App() {
   return (
     <>
@@ -27,7 +19,7 @@ function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
-        <Route path="/portfoilio" element={<Portfolio />}></Route>
+        <Route path="/Portfolio" element={<Portfolio />}></Route>
         <Route path="/resume" element={<Resume />}></Route>
         <Route path="*" element={<ErrorPage />}></Route>
       </Routes>
