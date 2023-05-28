56d89599e76d9510b09a98c397e6766dda1529a6
Harperzhu <harperzhu@yahoo.com>
2023-05-28 15:56:08 -0700

    modify firebase 404 page

--- frontend/personal-website/src/components/ResumeDownloadPage.js
+++ frontend/personal-website/src/components/ResumeDownloadPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import SideBar from "./SideBar";
 import NavBar from "./NavBar";
+import cat_high_five_gif from "../assets/images/resumeDownloadImage/cat-high-five.gif";
 
 export default function ResumeDownloadPage() {
   const navigate = useNavigate();
@@ -21,10 +22,17 @@ export default function ResumeDownloadPage() {
         <NavBar />
         <article className="about active" data-page="about">
           <div className="error-page">
-            <h1 className="extra-small-heading">Resume Downloaded!</h1>
-            <p className="extra-small-paragraph">
+            <img
+              src={cat_high_five_gif}
+              alt="cat high fiving your resume download!"
+              style={{ width: "360px", height: "300px" }}
+            />
+            <h1 className="extra-small-heading" style={{ fontSize: "1.5rem" }}>
+              Success!
+            </h1>
+            <p className="extra-small-paragraph" style={{ fontSize: "1.3rem" }}>
               Harper's resume has been successfully downloaded. If the download
-              did not start in a few seconds, you can click the link below to
+              do not start in a few seconds, you can click the link below to
               download it.
             </p>
             <button
@@ -35,13 +43,16 @@ export default function ResumeDownloadPage() {
                 href="https://drive.google.com/uc?export=download&id=1afC3oW_-AohjKp9KrEqWayemyZ06hPYV"
                 download
                 className="resume-download-link"
+                style={{ fontSize: "1.2rem", color: "#FFDB70" }}
               >
                 Download Resume
               </a>
             </button>
+            <br></br>
             <button
               onClick={goBack}
               className="error-button extra-small-button"
+              style={{ fontSize: "1.2rem" }}
             >
               Back to Main Page
             </button>
--- frontend/personal-website/src/components/subcomponents/MessageReceived.js
+++ frontend/personal-website/src/components/subcomponents/MessageReceived.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import NavBar from "../NavBar";
+import cat_hat_off from "../../assets/images/messageReceivedImage/cat-hat-off.gif";
 
 export default function MessageReceived() {
   const navigate = useNavigate();
@@ -14,12 +15,22 @@ export default function MessageReceived() {
       <NavBar />
       <article className="about active" data-page="about">
         <div className="error-page">
-          <h1>Thank You!</h1>
-          <p>
-            Your message has been received successfully. We appreciate your
-            feedback!
+          <br></br>
+
+          <img
+            src={cat_hat_off}
+            alt="cat taking hat off as a thank you gesture!"
+            style={{ width: "300px", height: "300px" }}
+          />
+          <h1 style={{ fontSize: "1.5rem" }}>Thank You!</h1>
+          <p style={{ fontSize: "1rem" }}>
+            Your message has been received successfully.
           </p>
-          <button onClick={goBack} className="error-button">
+          <button
+            onClick={goBack}
+            className="error-button"
+            style={{ fontSize: "0.9rem" }}
+          >
             Back to Main Page
           </button>
         </div>
