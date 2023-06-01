5f1f73344f2463faffe15cbd40ec84c622d26217
Harperzhu <harperzhu@yahoo.com>
2023-05-31 19:05:10 -0700

    the progress bar animation half done

--- frontend/personal-website/src/components/resumeComponents/SkillBar.js
+++ frontend/personal-website/src/components/resumeComponents/SkillBar.js
@@ -1,64 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function SkillBar() {
+  const [values, setValues] = useState([]);
+
+  let skillPercentDic = {
+    JavaScript: 60,
+    Python: 60,
+    JAVA: 50,
+    C: 50,
+    R: 30,
+  };
+  let skillName = Object.keys(skillPercentDic);
+  let skillPercent = Object.values(skillPercentDic);
+
+  useEffect(() => {
+    const newValues = skillPercent.map((percent) => percent * 2); // Adjust the multiplier to make the bars shorter
+    setValues(newValues);
+  }, [skillPercent]);
+
   return (
     <section className="skill animate-skill-bars">
       <h3 className="h3 skills-title">My skills</h3>
 
       <ul>
-        <li className="skills-item">
-          <div className="title-wrapper">
-            <h5 className="h5">Python</h5>
-            <data value="80">60%</data>
-          </div>
-
-          <div className="skill-progress-bg">
-            <div className="skill-progress-fill" style={{ width: "60%" }}></div>
-          </div>
-        </li>
-
-        <li className="skills-item">
-          <div className="title-wrapper">
-            <h5 className="h5">JavaScript</h5>
-            <data value="70">60%</data>
-          </div>
-
-          <div className="skill-progress-bg">
-            <div className="skill-progress-fill" style={{ width: "60%" }}></div>
-          </div>
-        </li>
-
-        <li className="skills-item">
-          <div className="title-wrapper">
-            <h5 className="h5">Java</h5>
-            <data value="90">50%</data>
-          </div>
-
-          <div className="skill-progress-bg">
-            <div className="skill-progress-fill" style={{ width: "50%" }}></div>
-          </div>
-        </li>
-
-        <li className="skills-item">
-          <div className="title-wrapper">
-            <h5 className="h5">R</h5>
-            <data value="50">50%</data>
-          </div>
-
-          <div className="skill-progress-bg">
-            <div className="skill-progress-fill" style={{ width: "50%" }}></div>
-          </div>
-        </li>
-        <li className="skills-item">
-          <div className="title-wrapper">
-            <h5 className="h5">C</h5>
-            <data value="70">30%</data>
-          </div>
-
-          <div className="skill-progress-bg">
-            <div className="skill-progress-fill" style={{ width: "30%" }}></div>
-          </div>
-        </li>
+        {skillName.map((name, index) => (
+          <li className="skills-item" key={name}>
+            <div className="title-wrapper">
+              <h5 className="h5">{name}</h5>
+              <data value={skillPercent[index]}>{skillPercent[index]}</data>
+            </div>
+            <div className="skill-progress-bg">
+              <div
+                className="skill-progress-fill"
+                style={{
+                  width: values[index] ? `${0.5 * values[index]}%` : "0%",
+                }}
+              ></div>
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   );
