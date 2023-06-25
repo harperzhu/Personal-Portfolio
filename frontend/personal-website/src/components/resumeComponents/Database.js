ba56b6f70c5d4a5241505b2ed25ce2c6ba164798
Harperzhu <harperzhu@yahoo.com>
2023-06-24 23:49:33 -0700

    update logo with elastic search

--- frontend/personal-website/src/components/resumeComponents/Database.js
+++ frontend/personal-website/src/components/resumeComponents/Database.js
@@ -2,6 +2,7 @@ import React from "react";
 import mongoDBIcon from "../../assets/images/databaseImage/mongodb-icon.svg";
 import postgresqlIcon from "../../assets/images/databaseImage/postgresql-icon.svg";
 import mySqlIcon from "../../assets/images/databaseImage/mysql-icon.svg";
+import elasticSearchIcon from "../../assets/images/databaseImage/elasticsearch-icon.svg";
 
 export default function Database() {
   return (
@@ -11,6 +12,16 @@ export default function Database() {
         <h3 className="h3 skills-title">Database </h3>
         <img
           src={mongoDBIcon}
+          alt="mongoDB"
+          style={{
+            width: "62px",
+            height: "62px",
+            display: "inline-block",
+            // backgroundColor: "white",
+          }}
+        />{" "}
+        <img
+          src={elasticSearchIcon}
           alt="mongoDB"
           style={{
             width: "62px",
